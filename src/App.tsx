4a1b49e86ce299238d298afb1e642e7eaa5d079b
Tchomasek <tchom.asek@centrum.cz>
2025-10-19 13:19:53 +0200

    update coins based on data from websocket

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 import useWebSocket from "react-use-websocket";
 import "./App.css";
 
-const coingeckoApiKey = process.env.REACT_APP_COINGECKO_API_KEY;
-const finnhubApiKey = process.env.REACT_APP_FINNHUB_API_KEY;
+const COINGECKO_API_KEY = process.env.REACT_APP_COINGECKO_API_KEY;
+const FINNHUB_API_KEY = process.env.REACT_APP_FINNHUB_API_KEY;
+
+const NUMBER_OF_COINS = 10;
 
 interface Coin {
   id: string;
@@ -12,18 +14,54 @@ interface Coin {
   name: string;
   current_price: number;
   image: string;
+  price_change_percentage_24h: number;
+  price_change_direction?: "up" | "down" | "none";
 }
 
 function App() {
   const [coins, setCoins] = useState<Coin[]>([]);
   const { sendMessage, readyState } = useWebSocket(
-    `wss://ws.finnhub.io?token=${finnhubApiKey}`,
+    `wss://ws.finnhub.io?token=${FINNHUB_API_KEY}`,
     {
       onOpen: () => {
         console.log("connected");
       },
       onMessage: (event) => {
-        console.log(event.data);
+        const message = JSON.parse(event.data);
+        if (message.type === "trade" && message.data) {
+          setCoins((prevCoins) => {
+            const coinsMap = new Map(
+              prevCoins.map((coin) => [coin.symbol.toLowerCase(), coin])
+            );
+            message.data.forEach((trade: any) => {
+              const symbol = trade.s
+                .split(":")[1]
+                .replace("USDT", "")
+                .toLowerCase();
+              const existingCoin = coinsMap.get(symbol);
+              if (existingCoin && existingCoin.current_price !== trade.p) {
+                const direction =
+                  trade.p > existingCoin.current_price ? "up" : "down";
+                coinsMap.set(symbol, {
+                  ...existingCoin,
+                  current_price: trade.p,
+                  price_change_direction: direction,
+                });
+              }
+            });
+            return Array.from(coinsMap.values());
+          });
+
+          setTimeout(() => {
+            setCoins((prevCoins) =>
+              prevCoins.map((coin) =>
+                coin.price_change_direction
+                  ? { ...coin, price_change_direction: "none" }
+                  : coin
+              )
+            );
+          }, 1000);
+        }
       },
       shouldReconnect: (closeEvent) => true,
     }
@@ -33,7 +71,7 @@ function App() {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&x_cg_demo_api_key=${coingeckoApiKey}`
+          `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${NUMBER_OF_COINS}&page=1&sparkline=false&x_cg_demo_api_key=${COINGECKO_API_KEY}`
         );
         const data = await response.json();
         setCoins(data);
@@ -72,7 +110,16 @@ function App() {
         </thead>
         <tbody>
           {coins.map((coin) => (
-            <tr key={coin.id}>
+            <tr
+              key={coin.id}
+              className={
+                coin.price_change_direction === "up"
+                  ? "flash-green"
+                  : coin.price_change_direction === "down"
+                  ? "flash-red"
+                  : ""
+              }
+            >
               <td>{coin.market_cap_rank}</td>
               <td>
                 <img src={coin.image} alt={coin.name} width="30" />
